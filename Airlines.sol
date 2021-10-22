92f1d7f13b7d0113c33672a06b85a04318c582ac
Kerry <kerry.t.mui@gmail.com>
2021-10-22 07:48:15 -0400

    initial contract commit

---
+++ Airlines.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.0;
+
+
+contract Airlines {
+address chairperson;
+struct details (
+  uint escrow; // deposit for payment settlement
+  uint status;
+  uint hashOfDetails;
+)
+}
