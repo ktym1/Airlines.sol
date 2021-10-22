f5d121e444c410d5a8133640df6914af9a8d5700
Kerry <kerry.t.mui@gmail.com>
2021-10-22 07:56:17 -0400

    Added account payments and membership mapping + modifiers

--- Airlines.sol
+++ Airlines.sol
@@ -3,9 +3,29 @@ pragma solidity ^0.6.0;
 
 contract Airlines {
 address chairperson;
+
+
+// Airlines data structures
 struct details (
   uint escrow; // deposit for payment settlement
   uint status;
   uint hashOfDetails;
 )
+
+// Airline account payments and membership mapping
+mapping (address=>details) public balanceDetails;
+mapping (address=>uint) membership;
+
+
+//modifier for onlyChairperson rule
+modifier onlyChairperson{
+  require(msg.sender == chairperson);
+  _;
+}
+
+//modifier onlyMember{
+  require(membership[msg.sender]==1);
+  _;
+}
+
 }
